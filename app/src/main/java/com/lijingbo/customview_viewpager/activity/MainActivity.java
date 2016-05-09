8670ed90494397ec2b75299d3a3e40b9041793f2
jingbolee <jingbolee@foxmail.com>
2016-05-09 14:12:04 +0800

    1.修改版本管理内容 2.添加红色point的移动

--- app/src/main/java/com/lijingbo/customview_viewpager/activity/MainActivity.java
+++ app/src/main/java/com/lijingbo/customview_viewpager/activity/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.lijingbo.customview_viewpager.R;
 import com.lijingbo.customview_viewpager.utils.DensityUtil;
@@ -16,11 +19,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private ViewPager vp;
     private LinearLayout ll_guide_group;
     private List< ImageView > imagesLists;
+    private ImageView view_red_point;
     private int[] guideImages;
+    private int pointDis; //两个点之间的距离
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         vp = (ViewPager) findViewById(R.id.vp_guide);
         ll_guide_group = (LinearLayout) findViewById(R.id.ll_guide_group);
+        view_red_point = (ImageView) findViewById(R.id.view_red_point);
     }
 
     private void initData() {
@@ -60,10 +67,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+//        final ViewTreeObserver observer = ll_guide_group.getViewTreeObserver();
+//        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @TargetApi( Build.VERSION_CODES.JELLY_BEAN )
+//            @Override
+//            public void onGlobalLayout() {
+//                observer.removeOnGlobalLayoutListener(this);
+////                observer.removeGlobalOnLayoutListener(this);
+//                pointDis = ll_guide_group.getChildAt(1).getLeft() - ll_guide_group.getChildAt(0).getLeft();
+//                Log.e(TAG, "连个point的距离为：" + pointDis);
+//            }
+//        });
+
+        ll_guide_group.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                ll_guide_group.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                pointDis = ll_guide_group.getChildAt(1).getLeft() - ll_guide_group.getChildAt(0).getLeft();
+                Log.e(TAG, "连个point的距离为：" + pointDis);
+            }
+        });
+
     }
 
     private void initAdapter() {
         vp.setAdapter(new GuideAdapter());
+        vp.addOnPageChangeListener(new GuidePageChangeListener());
     }
 
 
@@ -88,6 +117,38 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
+        }
+    }
+
+    class GuidePageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            System.out.println("viewpager改变监听：" + position + ", " + positionOffset + ", " + positionOffsetPixels);
+            int offset = (int) (pointDis * positionOffset + position * pointDis);
+////            ViewGroup.LayoutParams params = view_red_point.getLayoutParams();
+//            int l = view_red_point.getLeft();
+//            int t = view_red_point.getTop();
+//            int r = view_red_point.getRight();
+//            int b = view_red_point.getBottom();
+//            System.out.println("view_red_point的左上下右：" + l + "," + t + "," + r + "," + b);
+////            view_red_point.layout(l + offset, t, r + offset, b);
+//            view_red_point.scrollTo(l + offset, t);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view_red_point.getLayoutParams();
+            params.leftMargin =offset;
+            view_red_point.setLayoutParams(params);
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
         }
     }
 
